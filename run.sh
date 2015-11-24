f66328af4a7ed0623c8de30e70d3bab76b8dd9cd
pti <pawel.tobiszewski@blstream.com>
2015-11-24 11:46:32 +0100

    modified script for running tests

--- run.sh
+++ run.sh
@@ -1,9 +1,21 @@
-#!/usr/bin/env bash
+# !/usr/bin/env bash
 adb logcat -c
 adb logcat V:* > logcat.txt &
-LOGCAT_PID=$!
-
-echo $LOGCAT_PID
+export LOGCAT_PID=$!
 
 chmod +x gradlew
-./gradlew --stacktrace cAT
+./gradlew --daemon --stacktrace cAT
+
+echo "BEFORE"
+ps -e | grep adb
+
+echo "LOGCAT PID $LOGCAT_PID. Killing..."
+kill $LOGCAT_PID
+unset LOGCAT_PID
+
+echo "AFTER"
+ps -e | grep adb
+
+wc -l logcat.txt >> log_sizes
+echo ''
+tail -n 10 log_sizes
